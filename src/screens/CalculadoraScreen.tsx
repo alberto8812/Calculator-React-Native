683e2d4ac07b02432e0f40a692d266a66ff52958
alberto8812 <cvelascosaavedra@gmail.com>
2023-09-06 21:51:34 -0500

    create all botton and opacity

--- src/screens/CalculadoraScreen.tsx
+++ src/screens/CalculadoraScreen.tsx
@@ -9,11 +9,36 @@ const CalculadoraScreen = () => {
       <Text style={styles.resultado}>15.000.000</Text>
       <Text style={styles.litleresult}>15.000.000</Text>
 
+      {/*filas de botones */}
       <View style={styles.fila}>
-        <BottonCal text="C" color="Grey"/>
-        <BottonCal  text="+/-" color="Grey"/>
-        <BottonCal  text="del" color="Orange"/>
-        <BottonCal  text="/" color="Orange"/>
+        <BottonCal text="C" color="#9b9b9b"/>
+        <BottonCal  text="+/-" color="#9b9b9b"/>
+        <BottonCal  text="del" color="#FF6427"/>
+        <BottonCal  text="/" color="#FF6427"/>
+      </View>
+      <View style={styles.fila}>
+        <BottonCal text="7" />
+        <BottonCal  text="8" />
+        <BottonCal  text="9" />
+        <BottonCal  text="X" color="#FF6427"/>
+      </View>
+      <View style={styles.fila}>
+        <BottonCal text="4" />
+        <BottonCal  text="5" />
+        <BottonCal  text="6" />
+        <BottonCal  text="-" color="#FF6427"/>
+      </View>
+      <View style={styles.fila}>
+        <BottonCal text="1" />
+        <BottonCal  text="2" />
+        <BottonCal  text="3" />
+        <BottonCal  text="+" color="#FF6427"/>
+      </View>
+      <View style={styles.fila}>
+        <BottonCal text="0"  ancho/>
+        <BottonCal  text="." />
+
+        <BottonCal  text="=" color="#FF6427"/>
       </View>
 
     </View>
